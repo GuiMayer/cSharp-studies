2a561cee9a3e85f805301a992e06a293e182c289
GuiMayer <gui.mayer.ds@gmail.com>
2024-04-14 16:08:32 -0300

    atividade fila realizada

--- Aulas Jefferson/Aula 2/Atividade - Pilha/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ Aulas Jefferson/Aula 2/Atividade - Pilha/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd1f6a4197d8704a09d7d4448b6628d33b7d479")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+899bc727cc4b671fb5ec2d6e99d2f199a2948b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
