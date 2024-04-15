5a0197b24c22f03349f5022f1f5b35d8e3cd61e2
GuiMayer <gui.mayer.ds@gmail.com>
2024-04-15 17:12:43 -0300

    criação do método getAge

--- Aulas Jefferson/Sistema de Registro de Estudantes/ConsoleApp/Classes/Estudante.cs
+++ Aulas Jefferson/Sistema de Registro de Estudantes/ConsoleApp/Classes/Estudante.cs
@@ -2,15 +2,32 @@ using System;
 
 class Student
 {
-    public int id;
+    private int id;
     public string name;
-    public DateTime birthDate;
+    private DateTime birthDate;
     private static int nextId = 1;
 
-    public Student(string name, DateTime birthDate)
+    public Student(string name, int dayBirth, int monthBirth, int yearBirth)
     {
-        this.name = name;
-        this.birthDate = birthDate;
         this.id = nextId++;
+        this.name = name;
+        this.birthDate = new DateTime(yearBirth, monthBirth, dayBirth);
+    }
+
+    public int GetAge()
+    {
+        DateTime now = DateTime.Now;
+        if ((int)birthDate.Day > (int)now.Day && (int)birthDate.Month > (int)now.Month){
+            return (int)now.Year - (int)birthDate.Year - 1;    
+        }
+        else{
+            return (int)now.Year - (int)birthDate.Year;
+        }
+        
+    }
+
+    public void SetBirthDate(int dayBirth, int monthBirth, int yearBirth)
+    {
+        this.birthDate = new DateTime(yearBirth, monthBirth, dayBirth);
     }
 }
--- Aulas Jefferson/Sistema de Registro de Estudantes/ConsoleApp/Program.cs
+++ Aulas Jefferson/Sistema de Registro de Estudantes/ConsoleApp/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student bruna = new Student("Bruna Camila Silveira", 20, 8, 2005);
+        Student guilherme = new Student("Guilherme Mayer dos Santos", 17, 2, 2005);
+        Console.WriteLine(bruna.GetAge());
+        Console.WriteLine(guilherme.GetAge());
+    }
+}
