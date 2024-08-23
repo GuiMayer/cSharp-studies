daeb2116ee0e6d61b2a3fe1d465b77c37a9d43e1
GuiMayer <gui.mayer.ds@gmail.com>
2024-08-23 17:54:40 -0300

    Criação do projeto ConsoleRPG

---
+++ Aulas Jefferson/ConsoleRPG/ConsoleRPG/Models/Utilidades.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades
+{
+    class P
+    {
+        public static void PrintLn(string texto)
+        {
+            for (int i = 0; i < texto.Length; i++)
+            {
+                Console.Write(texto[i]);
+                Thread.Sleep(50);
+            }
+            Console.WriteLine();
+        }
+        public static void PrintLn(string texto, int tempo)
+        {
+            for (int i = 0; i < texto.Length; i++)
+            {
+                Console.Write(texto[i]);
+                Thread.Sleep(tempo);
+            }
+            Console.WriteLine();
+        }public static void Print(string texto)
+        {
+            for (int i = 0; i < texto.Length; i++)
+            {
+                Console.Write(texto[i]);
+                Thread.Sleep(50);
+            }
+        }
+        public static void Print(string texto, int tempo)
+        {
+            for (int i = 0; i < texto.Length; i++)
+            {
+                Console.Write(texto[i]);
+                Thread.Sleep(tempo);
+            }
+        }
+    }
+}
---
+++ Aulas Jefferson/ConsoleRPG/ConsoleRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Utilidades;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        P.Print("Este texto é apenas um teste.");
+    }
+}
